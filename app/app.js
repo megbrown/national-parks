046f72e088acae14bf68a28aaccca1a9782f49c9
Megan B <brown.m.j@outlook.com>
2017-07-31 09:03:51 -0500

    initial files and config

---
+++ app/app.js
@@ -0,0 +1,30 @@
+"use strict";
+
+let parksApp = angular.module("ParksApp", ["ngRoute"])
+.constant("FirebaseUrl", "");
+
+parksApp.config( ($routeProvider) => {
+	$routeProvider
+	.when("/register", {
+		templateUrl: "partials/registration.html",
+		controller: "RegisterCtrl"
+	})
+	.when("/", {
+		templateUrl: "partials/login.html",
+		controller: "LoginCtrl"
+	})
+	.when("/forests", {
+		templateUrl: "partials/forests-list.html",
+		controller: "ForestsListCtrl"
+	})
+	.when("/forests/favorite", {
+		templateUrl: "partials/forests-fave.html",
+		controller: "ForestsFavesCtrl"
+	})
+	.when("/forests/:id", {
+		templateUrl: "partials/forests-single.html",
+		controller: "ForestsSingleCtrl"
+	})
+	.otherwise("/");
+
+});
